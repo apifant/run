bef1bced273e2413abf034259612544d13d4c0b9
Kai Bissell <kai.bissell@tribetech.de>
2025-06-03 18:25:27 +0200

    Update

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 "use strict";
+// 1. Mit ANSI Escape Codes (keine Dependencies)
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hallo");
+// 8. Kombiniert mit dem Elefanten
+function logApifant() {
+    const orange = '\x1b[38;5;202m';
+    const reset = '\x1b[0m';
+    console.log(`${orange}   
+      ___________        __  
+     |          /|\      |
+     |         / | \     |       A P I F A N T
+     |        /__|______|       Unsere Mission: Ihr Entwicklungsprojekt beschleunigen.
+     |   _____   |
+     |___|   |___|
+
+Findet Sie Mehr heraus auf https://apifant.com/
+${reset}`);
+}
+logApifant();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log("Hallo")
+// 1. Mit ANSI Escape Codes (keine Dependencies)
+
+// 8. Kombiniert mit dem Elefanten
+function logApifant() {
+    const orange = '\x1b[38;5;202m';
+    const reset = '\x1b[0m';
+
+    console.log(`${orange}   
+      ___________        __  
+     |          /|\      |
+     |         / | \     |       A P I F A N T
+     |        /__|______|       Unsere Mission: Ihr Entwicklungsprojekt beschleunigen.
+     |   _____   |
+     |___|   |___|
+
+Findet Sie Mehr heraus auf https://apifant.com/
+${reset}`);
+}
+
+logApifant();
